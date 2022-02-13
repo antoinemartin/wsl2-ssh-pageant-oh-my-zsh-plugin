cd51ab5c79a353b5f793157d422f8d484ad3ed6f
Antoine Martin <antoine@openance.com>
2022-02-13 22:17:01 +0000

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # wsl2-ssh-pageant-oh-my-zsh-plugin
-Oh My ZSH plugin to use your Yubikey stored GPG keys from WSL
+
+[oh-my-zsh plugin](https://github.com/robbyrussell/oh-my-zsh) to use your
+Yubikey stored GPG keys from WSL.
+
+This plugin implements the instructions of the
+[wsl2-ssh-pageant repo](https://github.com/BlackReloaded/wsl2-ssh-pageant) as a
+oh-my-zsh plugin.
+
+To create your certificate and keys, the most comprehensive guide is probably
+the [YubiKey Guide](https://github.com/drduh/YubiKey-Guide) from drduh. In this
+guide you will find information on how to backup properly your private keys.
+However, the WSL part is for WSL 1.
+
+[This other guide](https://jardazivny.medium.com/the-ultimate-guide-to-yubikey-on-wsl2-part-1-dce2ff8d7e45)
+on Medium is specific to Windows and contains a section on WSL2.
+
+This plugins avoids adding code to your `~/.zshrc` file when you're using
+`oh-my-zsh` on WSL.
+
+## Install
+
+Create a new directory in `$ZSH_CUSTOM/plugins` called `wsl2-ssh-pageant` and
+clone this repo into that directory. Note: it must be named `wsl2-ssh-pageant`
+or oh-my-zsh won't recognize that it is a valid plugin directory.
+
+```
+git clone --depth 1 https://github.com/antoinemartin/wsl2-ssh-pageant-oh-my-zsh-plugin $ZSH_CUSTOM/plugins/wsl2-ssh-pageant
+```
+
+## Usage
+
+Add `wsl2-ssh-pageant` to the `plugins=()` list in your `~/.zshrc` file and
+you're done.
---
+++ wsl2-ssh-pageant.plugin.zsh
@@ -0,0 +1,42 @@
+wsl2_ssh_pageant_bin="$HOME/.ssh/wsl2-ssh-pageant.exe"
+if [[ ! -f "${wsl2_ssh_pageant_bin}" ]]; then
+  windows_destination="/mnt/c/Users/Public/Downloads/wsl2-ssh-pageant.exe"
+  if [[ ! -f "${windows_destination}" ]]; then
+    wget -O "$windows_destination" "https://github.com/BlackReloaded/wsl2-ssh-pageant/releases/latest/download/wsl2-ssh-pageant.exe"
+    # Set the executable bit.
+    chmod +x "$windows_destination"
+  fi
+  # Symlink to linux for ease of use later
+  ln -s $windows_destination $wsl2_ssh_pageant_bin
+fi
+
+export SSH_AUTH_SOCK="$HOME/.ssh/agent.sock"
+if ! ss -a | grep -q "$SSH_AUTH_SOCK"; then
+  rm -f "$SSH_AUTH_SOCK"
+  if test -x "$wsl2_ssh_pageant_bin"; then
+    (setsid nohup socat UNIX-LISTEN:"$SSH_AUTH_SOCK,fork" EXEC:"$wsl2_ssh_pageant_bin" >/dev/null 2>&1 &)
+  else
+    echo >&2 "WARNING: $wsl2_ssh_pageant_bin is not executable."
+  fi
+fi
+
+export GPG_AGENT_SOCK="$HOME/.gnupg/S.gpg-agent"
+if ! ss -a | grep -q "$GPG_AGENT_SOCK"; then
+  rm -rf "$GPG_AGENT_SOCK"
+  windows_username=$(cmd.exe /c echo %USERNAME% 2>/dev/null)
+  # When gpg4win is installed with scoop, the pipe is in the local directory
+  if [ -d "/mnt/c/Users/$windows_username/AppData/Local/gnupg" ]; then
+    config_path="C\:/Users/$windows_username/AppData/Local/gnupg"
+  else
+    config_path="C\:/Users/$windows_username/AppData/Roaming/gnupg"
+  fi
+  
+  if test -x "$wsl2_ssh_pageant_bin"; then
+    (setsid nohup socat UNIX-LISTEN:"$GPG_AGENT_SOCK,fork" EXEC:"$wsl2_ssh_pageant_bin --gpgConfigBasepath ${config_path} --gpg S.gpg-agent" >/dev/null 2>&1 &)
+  else
+    echo >&2 "WARNING: $wsl2_ssh_pageant_bin is not executable."
+  fi
+  unset windows_username config_path
+fi
+
+unset wsl2_ssh_pageant_bin
